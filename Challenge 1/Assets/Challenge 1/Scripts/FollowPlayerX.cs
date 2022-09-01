0ae451a3fa00c9b3a326402de912ab4b00a14e71
Adam <70708407+23calva@users.noreply.github.com>
2022-09-01 12:26:58 -0400

    Upload Prototype 2 | Finished Challenge 1

--- Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     public Vector3 offset;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        offset = new Vector3(0f, 5f, -15f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = plane.transform.position + offset;
--- Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public GameObject Propeller;
+
+    public float Speed = 20f;
+    public float RotationSpeed = 60f;
 
     void Start()
     {
-
+        Propeller = GameObject.Find("Propeller");
     }
 
     void FixedUpdate()
     {
         // move the plane forward at a constant rate
-        transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * speed * Time.deltaTime);
+        transform.Translate( Vector3.forward * Speed * Time.deltaTime);
 
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Input.GetAxis("Horizontal") * Vector3.right * rotationSpeed * Time.deltaTime);
+        // tilt the plane left/right based on up/down keys
+        transform.Rotate(Input.GetAxis("Horizontal") * Vector3.up * RotationSpeed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/downkeys
+        transform.Rotate(Input.GetAxis("Vertical") * Vector3.right * RotationSpeed * Time.deltaTime);
+
+        Propeller?.transform.Rotate(Vector3.forward, 10f);
     }
 }
