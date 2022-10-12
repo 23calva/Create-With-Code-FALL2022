424ebf1d8952f60f629b72c4e9a316c01322df99
Adam <70708407+23calva@users.noreply.github.com>
2022-10-12 11:30:18 -0400

    Woops

--- Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,11 +5,6 @@ public class FollowPlayerX : MonoBehaviour
     public GameObject plane;
     public Vector3 offset;
 
-    void Start()
-    {
-        offset = new Vector3(0f, 5f, -15f);
-    }
-
     void Update()
     {
         transform.position = plane.transform.position + offset;
